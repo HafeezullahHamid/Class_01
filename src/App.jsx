6989556593931954a0b888da7061a0497c985ef1
HafeezullahHamid <hafeezullahhamid32@gmail.com>
2024-04-14 09:39:41 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>HAFEEZULLAH</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi alias
+        provident porro fugit natus quis, officia recusandae rem! Explicabo
+        animi dolorem iusto libero molestiae alias nostrum adipisci, ratione
+        officiis vel.
+      </p>
+    </>
+  );
+}
+
+export default App;
